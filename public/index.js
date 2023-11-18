e71bb85afae68ec7f026586eda332f01e4717ae0
Sune Simonsen <sune@we-knowhow.dk>
2023-11-18 21:21:04 +0100

    Loading prism

--- public/index.js
+++ public/index.js
@@ -3,13 +3,22 @@ import { routes } from "./routes.js";
 import { Router, Routing } from "@dependable/nano-router";
 import { createBrowserHistory } from "@nano-router/history";
 import { RootView } from "./components/RootView.js";
+import { ScriptLoader } from "./components/ScriptLoader.js";
 
 const history = createBrowserHistory();
 
 const router = new Router({ routes, history });
 
 render(html`
-  <${Routing} router=${router}>
-    <${RootView} />
-  <//>
+  <${Routing} router=${router}><${RootView} /><//>
+  <${ScriptLoader}
+    sources=${[
+      "https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/prism.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/components/prism-js-templates.js",
+    ]}
+  />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/themes/prism-okaidia.min.css"
+  />
 `);
