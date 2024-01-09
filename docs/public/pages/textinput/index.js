6f4d44398efb2d80b6b0c0c33ae2164a1670d949
Sune Simonsen <sune@we-knowhow.dk>
2024-01-09 08:01:06 +0100

    Added a filtering input

--- docs/public/pages/textinput/index.js
+++ docs/public/pages/textinput/index.js
@@ -8,7 +8,9 @@ import {
 } from "@dependable/styleguide";
 import DefaultInput from "./DefaultInput.js";
 import MediaInput from "./MediaInput.js";
+import ClearableInput from "./ClearableInput.js";
 import SearchInput from "./SearchInput.js";
+import FilterInput from "./FilterInput.js";
 import DisabledInput from "./DisabledInput.js";
 import ReadonlyInput from "./ReadonlyInput.js";
 
@@ -29,10 +31,20 @@ export default class Page {
       <${Example} src=${new URL("./MediaInput.js", import.meta.url)}>
         <${MediaInput} />
       <//>
+      <${Heading} level="3">Clearable input<//>
+      <p>An input capable of clearing the input.</p>
+      <${Example} src=${new URL("./ClearableInput.js", import.meta.url)}>
+        <${ClearableInput} />
+      <//>
       <${Heading} level="3">Search input<//>
       <p>A special input for search fields.</p>
       <${Example} src=${new URL("./SearchInput.js", import.meta.url)}>
         <${SearchInput} />
+      <//>
+      <${Heading} level="3">Filter input<//>
+      <p>A special input for filtering items.</p>
+      <${Example} src=${new URL("./FilterInput.js", import.meta.url)}>
+        <${FilterInput} />
       <//>
       <${Heading} level="3"> Disabled<//>
       <p>
