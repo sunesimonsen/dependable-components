73a9cf119c9b19b13e9600aea485342495b1bc46
Sune Simonsen <sune@we-knowhow.dk>
2024-02-29 07:58:08 +0100

    Make Bar border lighter

--- packages/components/src/Bar/v0/index.js
+++ packages/components/src/Bar/v0/index.js
@@ -6,7 +6,7 @@ const border = `var(--dc-bar-border-width, 1px)`;
 const styles = css`
   & {
     border-style: var(--dc-bar-border-style, solid);
-    border-color: var(--dc-bar-border-color, var(--dc-color-neutral-80));
+    border-color: var(--dc-bar-border-color, var(--dc-color-neutral-85));
     border-width: 0;
     background: var(--dc-bar-background, --dc-color-background);
   }
